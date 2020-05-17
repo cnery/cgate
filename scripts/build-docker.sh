41909f0e3d89418a8797eaf228d97d6580604e89
Dave Oxley <dave@daveoxley.co.uk>
2020-05-17 22:27:21 +1000

    Fix arch name for amd64 and remove access.txt.

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -5,10 +5,11 @@ set -eo pipefail
 curl -O https://updates.clipsal.com/ClipsalSoftwareDownload/mainsite/cis/technical/CGate/cgate-2.11.4_3251.zip && \
     unzip cgate-2.11.4_3251.zip && \
     rm -f cgate-2.11.4_3251.zip && \
-    rm -f cgate/tag/*
+    rm -f cgate/tag/* && \
+    rm -f cgate/config/access.txt
 
 docker build -t cnery/cgate:arm-$BUILDKITE_BUILD_NUMBER -f Dockerfile.arm .
-docker build -t cnery/cgate:x86-64-$BUILDKITE_BUILD_NUMBER -f Dockerfile.x86-64 .
+docker build -t cnery/cgate:amd64-$BUILDKITE_BUILD_NUMBER -f Dockerfile.amd64 .
 
 rm -rf cgate
 ./scripts/push-docker.sh
--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -4,10 +4,10 @@ set -eo pipefail
 
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWD
 docker push cnery/cgate:arm-$BUILDKITE_BUILD_NUMBER
-docker push cnery/cgate:x86-64-$BUILDKITE_BUILD_NUMBER
+docker push cnery/cgate:amd64-$BUILDKITE_BUILD_NUMBER
 
 docker tag cnery/cgate:arm-$BUILDKITE_BUILD_NUMBER cnery/cgate:arm-latest
 docker push cnery/cgate:arm-latest
 
-docker tag cnery/cgate:x86-64-$BUILDKITE_BUILD_NUMBER cnery/cgate:x86-64-latest
-docker push cnery/cgate:x86-64-latest
+docker tag cnery/cgate:amd64-$BUILDKITE_BUILD_NUMBER cnery/cgate:amd64-latest
+docker push cnery/cgate:amd64-latest
